39708b3a0117a80a800599d20365ce8fec9ae4e7
Harshita Batra <101546087+Harshita-Batra-1@users.noreply.github.com>
2022-05-22 15:45:27 +0530

    Revert "Updating files"

--- main.cpp
+++ main.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 int main()
 {
-
+// agar values given ho and input na karani ho
     int a=2;
     int b=4;
     int c=6;
     int d = (a+b+c)/3;
     cout<<d<<endl;
-             //4 students marks average - 3 methods
+                      //4 students marks average - 3 methods
 //Using Integer
     int m1,m2,m3,m4,avg;
     cout<<"Enter marks: " << endl;
     cin>> m1>> m2>> m3>> m4;
-  
+    //mistake to give like << m1, m2, m3, m4
     avg = (m1+m2+m3+m4)/4;
     cout<<"Average: "<<avg;
 
 //using arrays
-    int m[4], sum=0, avg,i; 
-    for (i =1; i<=4; i++) 
+    int m[4], sum=0, avg,i; // for n input kra lenge phle n
+    for (i =1; i<=4; i++) //() imp but end me ; nhi
     {
         cout<<"Enter marks: ";
         cin>>m[i];
